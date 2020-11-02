8fe4d0f95b399d307b2e4e1bfda3825ce351b5a6
Samara Soares <samsoaresit@gmail.com>
2020-11-01 23:50:46 -0500

    Added final files

--- scripts.js
+++ scripts.js
@@ -1,1 +1,16 @@
 /* Add your JavaScript to this file */
+var sub = function(){
+    var email = document.getElementById("email");
+    document.getElementsByClassName("btn")[1].addEventListener("click", function(x){
+        var e = email.nodeValue;
+        if(e == ""){
+            document.getElementsByClassName("message")[0].innerHTML = "Please enter a valid email address.";
+
+        }
+        else {
+            document.getElementsByClassName("message")[0].innerHTML = "Thank you! Your email address" + e + "has been added to our mailing list!"
+        }
+    })
+
+}
+window.onload = sub;
